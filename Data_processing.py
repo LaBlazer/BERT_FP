d102d43d73ce8d0803805d393e79f0e76d56096a
LBLZR_ <avojtus@gmail.com>
2022-05-05 02:20:06 +0200

    Update Data_processing.py

--- Data_processing.py
+++ Data_processing.py
@@ -35,7 +35,7 @@ def PT_data():
     :param get_c_d:
     :return:
     """
-    data = open('douban_data/train.txt', 'r').readlines()
+    data = open('ubuntu_data/train.txt', 'r').readlines()
     data = [sent.split('\n')[0].split('\t') for sent in data]
     y = [int(a[0]) for a in data]
     cr = [[sen for sen in a[1:]] for a in data]
@@ -44,19 +44,19 @@ def PT_data():
         if y[i]==1:
             crnew.append(crsingle)
     crnew=crnew
-    pickle.dump(crnew, file=open("douban_post_train.pkl", 'wb'))
+    pickle.dump(crnew, file=open("ubuntu_post_train.pkl", 'wb'))
 
 if __name__ == '__main__':
     #Fine_tuning data constuction
     #including tokenization step
-    bert_tokenizer = BertTokenizer.from_pretrained("bert-base-chinese",do_lower_case=True)
+    bert_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased",do_lower_case=True)
     special_tokens_dict = {'eos_token': '[eos]'}
     num_added_toks = bert_tokenizer.add_special_tokens(special_tokens_dict)
 
     train, test, dev = {}, {}, {}
-    train['y'], train['cr'] = FT_data('douban_data/train.txt', tokenizer=bert_tokenizer)
-    dev['y'], dev['cr'] = FT_data('douban_data/dev.txt', tokenizer=bert_tokenizer)
-    test['y'], test['cr']= FT_data('douban_data/test.txt',tokenizer=bert_tokenizer)
+    train['y'], train['cr'] = FT_data('ubuntu_data/train.txt', tokenizer=bert_tokenizer)
+    dev['y'], dev['cr'] = FT_data('ubuntu_data/valid.txt', tokenizer=bert_tokenizer)
+    test['y'], test['cr']= FT_data('ubuntu_data/test.txt',tokenizer=bert_tokenizer)
     #char_vocab = defaultdict(float)
     dataset = train, dev, test
     pickle.dump(dataset, open('douban_data/dataset_1M.pkl', 'wb'))
